dce62af28af4819702e3d2fb5220611c01dc23e5
uploadersoasis <72340531+uploadersoasis@users.noreply.github.com>
2023-07-08 13:10:15 -0500

    Update outlaw_scriptlets.js

--- outlaw_scriptlets.js
+++ outlaw_scriptlets.js
@@ -1,31 +1,31 @@
 'use strict';
 
-/// set-attr-any-val.js
-/// alias stav.js
+/// set-attr-any.js
+/// alias saa.js
 /// world ISOLATED
-// This scriptlet is NOT the same as the set-attr.js added in the native/default uBlock
-// Origin scriptlets.js file.
-// example.com##+js(stav, selector, attribute, value)
-// was example.com##+js(sa, attr, value, [selector])
-// changed to conform to format of native set-attr.js, but not conflict with its alias.
-
-function setAttrAnyVal(selector = '', token = '', attrValue = '', runAt = '') {
+// example.com##+js(saa, attr, value, [selector])
+function setAttrAny(
+	token = '',
+	attrValue = '',
+	selector = '',
+	runAt = '' 
+) {
 	if ( token === '' ) { return; }
 	const tokens = token.split(/\s*\|\s*/);
 	if ( selector === '' ) { selector = `[${tokens.join('],[')}]`; }
 	let timer;
-	const setattrany = () => {
-		timer = undefined;	
-		const nodes = document.querySelectorAll(selector);
-		try {
-			for (const node of nodes) {
-				for ( const attr of tokens ) {
-				      if ( attr !== attrValue) { 
-					   node.setAttribute(attr, attrValue);
-				      }	      
-				}
+	const setattr = () => {
+	timer = undefined;	
+	const nodes = document.querySelectorAll(selector);
+	try {
+		for (const node of nodes) {
+			for ( const attr of tokens ) {
+			      if ( attr !== attrValue) { 
+				   node.setAttribute(attr, attrValue);
+			      }	      
 			}
-		} catch { }
+		}
+	} catch { }
 	};
 	const mutationHandler = mutations => {
 	if ( timer !== undefined ) { return; }
@@ -34,31 +34,31 @@ function setAttrAnyVal(selector = '', token = '', attrValue = '', runAt = '') {
 	    const { type, addedNodes, removedNodes } = mutations[i];
 	    if ( type === 'attributes' ) { skip = false; }
 	    for ( let j = 0; j < addedNodes.length && skip; j++ ) {
-			if ( addedNodes[j].nodeType === 1 ) { skip = false; break; }
+		if ( addedNodes[j].nodeType === 1 ) { skip = false; break; }
 	    }
 	    for ( let j = 0; j < removedNodes.length && skip; j++ ) {
-			if ( removedNodes[j].nodeType === 1 ) { skip = false; break; }
+		if ( removedNodes[j].nodeType === 1 ) { skip = false; break; }
 	    }
 	}
 	if ( skip ) { return; }
-	timer = self.requestAnimationFrame(setattrany);
+	timer = self.requestAnimationFrame(setattr);
 	};
 	const start = ( ) => {
-		setattrany();
-		if ( /\bloop\b/.test(runAt) === false ) { return; }
-		const observer = new MutationObserver(mutationHandler);
-		observer.observe(document.documentElement, {
-		    attributes: true,
-		    attributeFilter: tokens,
-		    childList: true,
-		    subtree: true,
-		});
+	setattr();
+	if ( /\bloop\b/.test(runAt) === false ) { return; }
+	const observer = new MutationObserver(mutationHandler);
+	observer.observe(document.documentElement, {
+	    attributes: true,
+	    attributeFilter: tokens,
+	    childList: true,
+	    subtree: true,
+	});
 	};
 	if ( document.readyState !== 'complete' && /\bcomplete\b/.test(runAt) ) {
         self.addEventListener('load', start, { once: true });
-    } else if ( document.readyState !== 'loading' || /\basap\b/.test(runAt) ) {
+    	} else if ( document.readyState !== 'loading' || /\basap\b/.test(runAt) ) {
         start();
-    } else {
+    	} else {
         self.addEventListener('DOMContentLoaded', start, { once: true });
-    }
+    	}
 }
