676864c4a92c269f0aa05917bcdb8daec04b4f4a
uploadersoasis <72340531+uploadersoasis@users.noreply.github.com>
2023-07-09 11:59:20 -0500

    Update outlaw_scriptlets.js

--- outlaw_scriptlets.js
+++ outlaw_scriptlets.js
@@ -8,14 +8,6 @@
 // This scriptlet is NOT the same as the set-attr.js added in the native/default uBlock
 // Origin scriptlets.js file.
 // NOTES: This will NOT set a value if the attribute does not exist.
-//        Do NOT put a semicolon at the end of the inputted attr parameter even if it is a Javascript function.
-/*
-TODO: Do not treat attrValue as a string if want to use a variable or calculated value for the value.
-      How, as not setting attrValue to default string and converting it is not working?
-      In that case, only treat attrValue as string if enclosed in quotation marks.
-      document.querySelector('button[id="downloadbtn"]').attributes.onclick.value.slice(12, -2); removes windows.open() from the string value.
-      clicknupload.*##+js(saa, href, document.querySelector('button[id="downloadbtn"]').attributes.onclick.value.slice(12, -2), button[id="downloadbtn"])
-*/
 //function setAttrAny(token = '', attrValue = '', selector = '', run = '') {
 function setAttrAny(token = '', attrValue, selector = '', run = '') { // attrValue must be a string so convert below if not
 	if ( token === '' ) { return; }
@@ -30,7 +22,7 @@ function setAttrAny(token = '', attrValue, selector = '', run = '') { // attrVal
 		try {
 			for (const node of nodes) {
 				for ( const attr of tokens ) {
-					if ( attr !== attrValue) { 
+					if ( attr !== attrValue ) { 
 						node.setAttribute(attr, attrValue);
 					}
 				}
