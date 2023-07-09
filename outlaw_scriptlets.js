753e626a4bf850cca0d87fbedc3c2e7cd2aa7dda
uploadersoasis <72340531+uploadersoasis@users.noreply.github.com>
2023-07-09 09:51:14 -0500

    allow variable as inputted value to set for attribute

--- outlaw_scriptlets.js
+++ outlaw_scriptlets.js
@@ -7,11 +7,19 @@
 // example.com##+js(saa, attr, value, [selector])
 // This scriptlet is NOT the same as the set-attr.js added in the native/default uBlock
 // Origin scriptlets.js file.
-// NOTE: This will NOT set a value if the attribute does not exist.
+// NOTES: This will NOT set a value if the attribute does not exist.
+//        Do NOT put a semicolon at the end of the inputted attr parameter even if it is a Javascript function.
+/*
+TODO: Do not treat attrValue as a string if want to use a variable or calculated value for the value.
+      In that case, only treat attrValue as string if enclosed in quotation marks.
+      document.querySelector('button[id="downloadbtn"]').attributes.onclick.value.slice(12, -2); removes windows.open() from the string value.
+      clicknupload.*##+js(saa, href, document.querySelector('button[id="downloadbtn"]').attributes.onclick.value.slice(12, -2), button[id="downloadbtn"])
+*/
 function setAttrAny(token = '', attrValue = '', selector = '', run = '') {
 	if ( token === '' ) { return; }
 	const tokens = token.split(/\s*\|\s*/);
 	if ( selector === '' ) { selector = `[${tokens.join('],[')}]`; }
+	attrValue = String(attrValue);  // convert to string in case variable was inputted value
 	let timer;
 	const setattr = () => {
 		timer = undefined;	
